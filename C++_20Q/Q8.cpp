23c28e23745d7790e1aabc3d667c83f2013c352e
Misallocated <222233906+misallocated@users.noreply.github.com>
2025-11-27 05:57:24 +0000

    initial upload

--- C++_20Q/Q8.cpp
+++ C++_20Q/Q8.cpp
@@ -1,3 +1,36 @@
 #include <iostream>
 
-class LLNode {};
+class LLNode {
+
+public:
+  std::string payload;
+  LLNode *next;
+
+  LLNode(std::string payload, LLNode *next) {
+    this->payload = payload;
+    this->next = next;
+  }
+};
+
+int main() {
+  LLNode *base = nullptr;
+  LLNode *temp;
+
+  // Insert 0 node
+  temp = new LLNode("Zero", base);
+  base = temp;
+
+  // Head Insert First
+  temp = new LLNode("First", base);
+  base = temp;
+
+  // Head Insert Second
+  temp = new LLNode("Second", base);
+  base = temp;
+
+  LLNode *current = base;
+  while (current) {
+    std::cout << current->payload << std::endl;
+    current = current->next;
+  }
+}
